27c6da0bd517e6295046626e487ab13b564b46a1
Rashi25-pc <mechengineersoft.com>
2025-02-27 10:49:57 +0530

    Updated fetchData.js to use environment variables

--- netlify/functions/fetchData.js
+++ netlify/functions/fetchData.js
@@ -5,9 +5,20 @@ const spreadsheetId = '1aJEVYDgVxhXVpOZrc8-JvHtwDXrX3v77jNZwPOad0vY';
 const sheetName = 'Data';
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json', // Path to your JSON key file
-  scopes: SCOPES,
-});
+    credentials: {
+      type: 'service_account',
+      project_id: 'login-451104',
+      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    },
+    scopes: SCOPES,
+  });
 
 exports.handler = async (event, context) => {
   const { blockNo, partNo, thickness } = event.queryStringParameters;
